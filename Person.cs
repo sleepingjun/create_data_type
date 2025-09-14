dbddd593b19200a848dfc7629158a96500c7aac6
ani <best19980102@gmail.com>
2025-09-14 13:40:59 +0800

    Class, Object++

---
+++ Person.cs
@@ -0,0 +1,8 @@
+﻿//來創個class模板， 然後回Program.cs創出來
+
+class Person
+{
+    public string Name;//public 屬性
+    public int Age;
+    public double Height;
+}   
--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿// class :=自創新的資料型態
+// object :=萬物皆物件； 用class宣告的資料型態都是object
+
+Person p1 = new Person();//create object
+p1.Name = "John";
+p1.Age = 30;
+p1.Height = 1.75;
+Console.WriteLine($"Name: {p1.Name}, Age: {p1.Age}, Height: {p1.Height}m");
+Person p2 = new Person();
+p2.Name = "Alice";
+p2.Age = 25;
+p2.Height = 1.65;
+Console.WriteLine($"Name: {p2.Name}, Age: {p2.Age}, Height: {p2.Height}m");
+Person p3 = new Person();
+p3.Name = "Bob";
+p3.Age = 28;
+p3.Height = 1.88;
+Console.WriteLine($"Name: {p3.Name}, Age: {p3.Age}, Height: {p3.Height}m");
